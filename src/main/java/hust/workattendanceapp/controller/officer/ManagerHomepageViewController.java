42eb9aa63c574b8e6093d437cd05dc04d126fef5
rzngnam1402 <legiangnam1402@gmail.com>
2023-06-23 14:26:48 +0700

    fead: add sidebar

--- src/main/java/hust/workattendanceapp/controller/officer/ManagerHomepageViewController.java
+++ src/main/java/hust/workattendanceapp/controller/officer/ManagerHomepageViewController.java
@@ -1,4 +1,33 @@
 package hust.workattendanceapp.controller.officer;
 
+import hust.workattendanceapp.WorkAttendanceApplication;
+import hust.workattendanceapp.constraints.FXMLConstraints;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ManagerHomepageViewController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void switchToEditDataRequestsList(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(WorkAttendanceApplication.class.getResource(FXMLConstraints.EDIT_REQUEST_LIST_VIEW_FXML));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void logOut(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(WorkAttendanceApplication.class.getResource(FXMLConstraints.LOGIN_VIEW_FXML));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }

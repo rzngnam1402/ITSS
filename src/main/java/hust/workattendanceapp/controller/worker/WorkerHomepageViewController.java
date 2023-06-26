a919fd346ab329987a6dc7d8ac5a2ba74a75b62e
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-06-26 15:41:14 +0700

    fea: 20205070 fix worker view

--- src/main/java/hust/workattendanceapp/controller/worker/WorkerHomepageViewController.java
+++ src/main/java/hust/workattendanceapp/controller/worker/WorkerHomepageViewController.java
@@ -1,4 +1,26 @@
 package hust.workattendanceapp.controller.worker;
 
+import hust.workattendanceapp.WorkAttendanceApplication;
+import hust.workattendanceapp.constraints.FXMLConstraints;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class WorkerHomepageViewController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToPersonalAttendance(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(WorkAttendanceApplication.class.getResource(FXMLConstraints.PERSONAL_ATTENDANCE));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }

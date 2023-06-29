651ce9cae36f9d46b6cfbfc9777c62844a8b9e77
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-06-30 06:25:50 +0700

    refactor: 20205070 refactor code

--- src/test/java/hust/workattendanceapp/controller/worker/EditDataRequestPopUpControllerTest.java
+++ src/test/java/hust/workattendanceapp/controller/worker/EditDataRequestPopUpControllerTest.java
@@ -1,6 +1,5 @@
 package hust.workattendanceapp.controller.worker;
-
-import hust.workattendanceapp.model.worker.EditDataRequestForm;
+import hust.workattendanceapp.validate.TimeFormatValidate;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -10,48 +9,48 @@ public class EditDataRequestPopUpControllerTest {
     @Test
         // valid time format
     void tc1() {
-        assertFalse(EditDataRequestForm.checkDataError("00:00", "13:23", "2", null));
+        assertFalse(TimeFormatValidate.checkDataError("00"));
     }
 
     // valid time format
     @Test
     void tc2() {
-        assertTrue(EditDataRequestForm.checkDataError("10:00", "11:00", "17:00", "19:00"));
+        assertTrue(TimeFormatValidate.checkDataError("10:00"));
     }
 
     // Valid hour boundary
     @Test
     void tc3() {
-        assertTrue(EditDataRequestForm.checkDataError("13:00", "14:15", "19:00", "20:00"));
+        assertTrue(TimeFormatValidate.checkDataError("13:00"));
     }
 
     // Invalid hour boundary
     @Test
     void tc4() {
-        assertFalse(EditDataRequestForm.checkDataError("13:00", "14:00", "20:00", "24:00"));
+        assertFalse(TimeFormatValidate.checkDataError("24:00"));
     }
 
     // Invalid minute
     @Test
     void tc5() {
-        assertFalse(EditDataRequestForm.checkDataError("13:66", "15:77", "20:00", "23:61"));
+        assertFalse(TimeFormatValidate.checkDataError("23:61"));
     }
 
     // Invalid minute
     @Test
     void tc6() {
-        assertFalse(EditDataRequestForm.checkDataError("15:66", "15:54", "23:00", "22:61"));
+        assertFalse(TimeFormatValidate.checkDataError("15:66"));
     }
 
     // Invalid String
     @Test
     void tc7() {
-        assertFalse(EditDataRequestForm.checkDataError("13:66", "hello", "20:00", "23:61"));
+        assertFalse(TimeFormatValidate.checkDataError("abc abc"));
     }
 
     @Test
     void tc8() {
-        assertFalse(EditDataRequestForm.checkDataError("13:66", null, "20:00", "23:61"));
+        assertFalse(TimeFormatValidate.checkDataError(null));
     }
 
 

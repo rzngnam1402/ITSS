882ccc2e878c14a872b76810d06075eb2ceab1cf
Bui Minh Dung <85019719+junzmint@users.noreply.github.com>
2023-06-30 06:28:46 +0700

    refactor: 20205070 refactor code

--- src/test/java/hust/workattendanceapp/controller/worker/TimeFormatValidateTest.java
+++ src/test/java/hust/workattendanceapp/controller/worker/TimeFormatValidateTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class EditDataRequestPopUpControllerTest {
+public class TimeFormatValidateTest {
     @Test
         // valid time format
     void tc1() {
